0350513558d80c9ec5b41267d04b5b1c9945362a
agatasnoske40122 <agatasnoske40122@gmail.com>
2025-06-03 23:40:28 +0900

    パス修正

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -4,7 +4,7 @@ function Top() {
   return (
     <div className="main fade">
       <h1 className="top-logo">
-        <img src="../public/TOP_logo.svg" alt="SONOSUKE AGATA's portfolio" />
+        <img src="/TOP_logo.svg" alt="SONOSUKE AGATA's portfolio" />
       </h1>
     </div>
   )
